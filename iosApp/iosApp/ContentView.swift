fb080ad719b3d4ed909cb4f542afd5965241d14f
rhenwinch <rhenwinchester@gmail.com>
2026-03-25 13:41:21 +0800

    revert(ios): remove vibe coded work

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,32 +1,8 @@
 import SwiftUI
 
-private enum Route: Hashable {
-    case favourites
-}
-
 struct ContentView: View {
-    @State private var path: [Route] = []
-
-    @StateObject private var breedsViewModel = BreedsScreen.ViewModelWrapper()
-    @StateObject private var favouritesViewModel = FavouritesScreen.ViewModelWrapper()
-
     var body: some View {
-        NavigationStack(path: $path) {
-            BreedsScreen(
-                viewModel: breedsViewModel,
-                onNavigateToFavourites: { path.append(.favourites) }
-            )
-            .toolbarVisibility(.hidden, for: .navigationBar)
-            .navigationDestination(for: Route.self) { route in
-                switch route {
-                case .favourites:
-                    FavouritesScreen(
-                        viewModel: favouritesViewModel
-                    )
-                    .toolbarVisibility(.hidden, for: .navigationBar)
-                }
-            }
-        }
+        
     }
 }
 
